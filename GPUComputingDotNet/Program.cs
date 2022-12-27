8cbf58074c4e083905ced4cd0caa24476a3bdc25
WillianYamakawa <willianysouza2003@gmail.com>
2022-12-27 13:18:33 -0300

    Add project files.

---
+++ GPUComputingDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPUComputing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Fala");
+        }
+    }
+}
