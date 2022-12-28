5b4126e3265b6a2373053d301fa5e54b9aad74db
WillianYamakawa <willianysouza2003@gmail.com>
2022-12-28 13:52:52 -0300

    addons

--- GPUComputing/src/Source.cpp
+++ GPUComputing/src/Source.cpp
@@ -1,6 +1,24 @@
 #include <CL/cl.h>
 #include <cassert>
 #include <iostream>
+#include <fstream>
+
+static std::string read_file(const char* fileName) {
+	std::fstream f;
+	f.open(fileName, std::ios_base::in);
+	assert(f.is_open());
+
+	std::string res;
+	while (!f.eof()) {
+		char c;
+		f.get(c);
+		res += c;
+	}
+
+	f.close();
+
+	return std::move(res);
+}
 
 int main() {
 	cl_platform_id platforms[64];
@@ -27,26 +45,92 @@ int main() {
 		}
 	}
 
-	assert(device != nullptr);
-
-	cl_int clContextResult;
-	cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &clContextResult);
-	assert(clContextResult == CL_SUCCESS);
+	cl_int contextResult;
+	cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &contextResult);
+	assert(contextResult == CL_SUCCESS);
 
 	cl_int commandQueueResult;
 	cl_command_queue queue = clCreateCommandQueue(context, device, 0, &commandQueueResult);
 	assert(commandQueueResult == CL_SUCCESS);
 
-	const char* programSource = "";
-	size_t length;
-	cl_int createProgramResult;
-	cl_program program = clCreateProgramWithSource(context, 1, &programSource, &length, &createProgramResult);
-	assert(createProgramResult == CL_SUCCESS);
+	std::string s = read_file("C:\\Users\\willi\\Desktop\\vec.txt");
+	const char* programSource = s.c_str();
+	size_t length = 0;
+	cl_int programResult;
+	cl_program program = clCreateProgramWithSource(context, 1, &programSource, &length, &programResult);
+	assert(programResult == CL_SUCCESS);
 
 	cl_int programBuildResult = clBuildProgram(program, 1, &device, "", nullptr, nullptr);
+	if (programBuildResult != CL_SUCCESS) {
+		char log[256];
+		size_t logLength;
+		cl_int programBuildInfoResult = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 256, log, &logLength);
+		assert(programBuildInfoResult == CL_SUCCESS);
+		std::cout << log << std::endl;
+		assert(log);
+	}
 
+	cl_int kernelResult;
+	cl_kernel kernel = clCreateKernel(program, "vector_sum", &kernelResult);
+	assert(kernelResult == CL_SUCCESS);
 
+	float vecaData[256];
+	float vecbData[256];
 
+	for (int i = 0; i < 256; ++i) {
+		vecaData[i] = (float)(i * i);
+		vecbData[i] = (float)i;
+	}
+
+	cl_int vecaResult;
+	cl_mem veca = clCreateBuffer(context, CL_MEM_READ_ONLY, 256 * sizeof(float), nullptr, &vecaResult);
+	assert(vecaResult == CL_SUCCESS);
+
+	cl_int enqueueVecaResult = clEnqueueWriteBuffer(queue, veca, CL_TRUE, 0, 256 * sizeof(float), vecaData, 0, nullptr, nullptr);
+	assert(enqueueVecaResult == CL_SUCCESS);
+
+	cl_int vecbResult;
+	cl_mem vecb = clCreateBuffer(context, CL_MEM_READ_ONLY, 256 * sizeof(float), nullptr, &vecbResult);
+	assert(vecbResult == CL_SUCCESS);
+
+	cl_int enqueueVecbResult = clEnqueueWriteBuffer(queue, vecb, CL_TRUE, 0, 256 * sizeof(float), vecbData, 0, nullptr, nullptr);
+	assert(enqueueVecbResult == CL_SUCCESS);
+
+	cl_int veccResult;
+	cl_mem vecc = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 256 * sizeof(float), nullptr, &veccResult);
+	assert(veccResult == CL_SUCCESS);
+
+	cl_int kernelArgaResult = clSetKernelArg(kernel, 0, sizeof(cl_mem), &veca);
+	assert(kernelArgaResult == CL_SUCCESS);
+	cl_int kernelArgbResult = clSetKernelArg(kernel, 1, sizeof(cl_mem), &vecb);
+	assert(kernelArgaResult == CL_SUCCESS);
+	cl_int kernelArgcResult = clSetKernelArg(kernel, 2, sizeof(cl_mem), &vecc);
+	assert(kernelArgaResult == CL_SUCCESS);
+
+	size_t globalWorkSize = 256;
+	size_t localWorkSize = 64;
+	cl_int enqueueKernelResult = clEnqueueNDRangeKernel(queue, kernel, 1, 0, &globalWorkSize, &localWorkSize, 0, nullptr, nullptr);
+	assert(enqueueKernelResult == CL_SUCCESS);
+
+	float veccData[256];
+	cl_int enqueueReadBufferResult = clEnqueueReadBuffer(queue, vecc, CL_TRUE, 0, 256 * sizeof(float), veccData, 0, nullptr, nullptr);
+	assert(enqueueReadBufferResult == CL_SUCCESS);
+
+	clFinish(queue);
+
+	std::cout << "Result: ";
+	for (int i = 0; i < 256; ++i) {
+		std::cout << veccData[i] << std::endl;
+	}
+
+	clReleaseMemObject(veca);
+	clReleaseMemObject(vecb);
+	clReleaseMemObject(vecc);
+	clReleaseKernel(kernel);
+	clReleaseProgram(program);
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
+	clReleaseDevice(device);
 	
 
 	return 0;
